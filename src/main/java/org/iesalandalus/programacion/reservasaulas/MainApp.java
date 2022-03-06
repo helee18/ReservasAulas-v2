54f6fe1f4394a628baf70305c9f44fd7dbe3c817
heleven-pc <heleven-pc@192.168.22.11>
2022-03-06 21:38:43 +0100

    aplicacion principal

--- src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
@@ -1,11 +1,23 @@
 package org.iesalandalus.programacion.reservasaulas;
 
-
+import org.iesalandalus.programacion.reservasaulas.mvc.controlador.Controlador;
+import org.iesalandalus.programacion.reservasaulas.mvc.modelo.IModelo;
+import org.iesalandalus.programacion.reservasaulas.mvc.modelo.Modelo;
+import org.iesalandalus.programacion.reservasaulas.mvc.vista.IVista;
+import org.iesalandalus.programacion.reservasaulas.mvc.vista.Vista;
 
 public class MainApp {
 
 	public static void main(String[] args) {
+		System.out.println("Programa para la gestión de reservas de espacios del IES Al-Ándalus");
 		
+		IModelo modelo;
+		IVista vista;
+		
+		vista = new Vista();
+		modelo = new Modelo();
+		
+		new Controlador(modelo, vista);
 	}
 
 }
